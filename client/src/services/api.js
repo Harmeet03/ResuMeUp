f4f05b86ebd684a4d0ea31fa794d77853c1a92d3
Harmeet Singh <hsdhanjal2003@gmail.com>
2025-07-08 00:43:19 +0530

    FINAL UPDATE

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    // baseURL: 'http://localhost:5000',
+    baseURL: 'https://resumeup-7o8o.onrender.com/',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
