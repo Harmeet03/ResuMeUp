2aab08de40a7c57d5f6bdc95e5bfb31fe024c77a
Harmeet Singh <hsdhanjal2003@gmail.com>
2026-07-01 17:47:31 +0530

    Proxy trust

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,10 +2,6 @@ import User from "../models/User.js";
 import jwt from "jsonwebtoken"; 
 
 const protect = async (req, res, next) => {
-    
-    console.log("Cookie header:", req.headers.cookie);
-    console.log("Parsed cookies:", req.cookies);
-
     const token = req.cookies?.token;
 
     if(!token){
