1d333a4cc2d0c82b869f5ed6eb1260feaa61be3f
Harmeet Singh <hsdhanjal2003@gmail.com>
2026-07-01 17:21:17 +0530

    Proxy trust

--- server/server.js
+++ server/server.js
@@ -19,6 +19,8 @@ database()
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set("trust proxy", 1);
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser())
